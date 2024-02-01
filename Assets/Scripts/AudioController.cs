7b4a5b6488b86c11aac483c9b4298ad443a89039
icansingh <ikjyot@ualberta.ca>
2024-02-01 15:32:51 -0700

    Created an AudioController
    Created an audio controller similar to the one in Lab 4 (refer to that for more explanations).
    Created a playRandomSound function for playing barks every 10 seconds for the birds. For now, I imported 2 test chirps.

---
+++ Assets/Scripts/AudioController.cs
@@ -0,0 +1,47 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    private static AudioController _instance;
+    public static AudioController Instance {get {return _instance;}}
+    
+    // Initialize audio source and audio clips
+    public AudioSource chicken;
+    public AudioClip chickenHit, chickenJump, chickenGrassWalk, chickenCloudWalk;
+    public AudioClip[] chickenBarks;
+
+    void Awake(){
+        _instance = this;
+    }
+
+    // Use the following functions in other scripts to call
+    // desired sound effect
+    public void PlayChickenHit(){
+        chicken.PlayOneShot(chickenHit);
+    }
+
+    public void PlayChickenJump(){
+        chicken.PlayOneShot(chickenJump);
+    }
+
+    public void PlayChickenGrassWalk(){
+        chicken.PlayOneShot(chickenGrassWalk);
+    }
+
+    public void PlayChickenCloudWalk(){
+        chicken.PlayOneShot(chickenCloudWalk);
+    }
+
+    // Plays a bark every 10 seconds 
+    public void playRandomSound(){
+        Invoke ("PlayChickenBarks", 10);
+    }
+
+    public void PlayChickenBarks(){
+        chicken.PlayOneShot(chickenBarks[Random.Range(0, chickenBarks.Length)]);
+        playRandomSound();
+    }
+}
