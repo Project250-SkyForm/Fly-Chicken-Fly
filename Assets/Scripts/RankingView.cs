78783a335875d359ca672e80d3dcf73e5d91a570
Jimmy <zhijianliu72@gmail.com>
2024-02-04 02:10:19 -0700

    Add a scene for the start page and scripts for data management

---
+++ Assets/Scripts/RankingView.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Import the UI namespace, this is used to find out the text component
+
+public class RankingView : MonoBehaviour
+{
+    private int highestScore;
+    private Text text;
+
+     //Singleton pattern
+    private static RankingView _instance;
+    public static RankingView Instance { get { return _instance; } }
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+        highestScore = DataManager.Instance.getHighestScore();
+        setScores(highestScore);
+    }
+
+     void Awake(){
+        _instance = this;
+    }
+
+    public void setScores(int highest){
+        text.text = "Highest Scores: " + highest;
+    }
+}
---
+++ Assets/Scripts/SceneController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    //Singleton pattern
+    private static SceneController _instance;
+    public static SceneController Instance { get { return _instance; } }
+
+
+    void Awake(){
+        _instance = this;
+    }
+
+    public void ChangeScene(string targetSceneName)
+    {
+        Debug.Log(targetSceneName);
+        
+        SceneManager.LoadScene(targetSceneName);
+    }
+}
