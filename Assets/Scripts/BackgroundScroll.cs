2850508efd3cf03fc2c57343eaeb14f7fc2c62a5
Jimmy <zhijianliu72@gmail.com>
2024-02-27 21:05:54 -0700

    Add other background, loop background

--- Assets/Scripts/BackgroundScroll.cs
+++ Assets/Scripts/BackgroundScroll.cs
@@ -7,20 +7,30 @@ public class BackgroundScroll : MonoBehaviour
     public float const_speed;
     public float speed; // control scroll speed from unity, for the camera itis -1, background is -0.8
     private Vector3 StartPos; // initial starting position
+    public GameObject loopBG;
     public SpriteRenderer spriteRenderer; // get the sprite renderer of the background
 
+
+    
     // Start is called before the first frame update
     void Start()
     {
         StartPos = transform.position; // get initial starting pos of background
         speed = 0;
-        spriteRenderer = GetComponent<SpriteRenderer>(); // get the sprite renderer of the background
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        Continue();
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(translation: Vector3.down * speed * Time.deltaTime); // move bg based on speed
+        if (transform.position.y<-100){
+            SpriteRenderer loopBGRender = loopBG.GetComponent<SpriteRenderer>(); // get the sprite renderer of the background
+            Vector3 newPos = loopBGRender.transform.position;
+            newPos.y +=200;
+            transform.position = newPos;
+        }
     }
 
     public void Stop(){
