56d41533fcd0fbe5f7f9ef4de7c34f35f2e7f904
Jimmy <zhijianliu72@gmail.com>
2024-02-29 00:59:29 -0700

    update UI views

--- Assets/Scripts/LocalRankView.cs
+++ Assets/Scripts/LocalRankView.cs
@@ -3,29 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI; // Import the UI namespace, this is used to find out the text component
 
-public class RankingView : MonoBehaviour
+public class LocalRankView : MonoBehaviour
 {
     private int highestScore;
+    public int rank;
     private Text text;
-
-     //Singleton pattern
-    private static RankingView _instance;
-    public static RankingView Instance { get { return _instance; } }
 
     // Start is called before the first frame update
 
     void Start()
     {
         text = gameObject.GetComponent<Text>();
-        highestScore = DataManager.Instance.getHighestScore(0);
+        highestScore = DataManager.Instance.getHighestScore(rank-1);
         setScores(highestScore);
     }
 
-     void Awake(){
-        _instance = this;
-    }
-
     public void setScores(int highest){
-        text.text = "Highest Scores: " + highest + " M";
+        text.text =  highest + " m";
     }
 }
