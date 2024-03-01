2e1ef2d2029a476681902fd6b651789d23affb81
Jimmy <zhijianliu72@gmail.com>
2024-03-01 15:54:23 -0700

    update video scene

---
+++ Assets/Scripts/VideoScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.SceneManagement;
+
+public class VideoScript : MonoBehaviour
+{
+    public VideoPlayer videoPlayer;
+    public string nextSceneName;
+
+    void Start()
+    {
+        videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    void OnVideoEnd(VideoPlayer vp)
+    {
+        if (vp == videoPlayer)
+        {
+            // Load the next scene when the video ends
+            SceneController.Instance.ChangeScene(nextSceneName);
+        }
+    }
+}
