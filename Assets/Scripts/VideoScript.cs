91165647b1cc5583421a9b9f0a33678ff437cdc0
icansingh <ikjyot@ualberta.ca>
2024-03-01 21:55:49 -0700

    WebGL video support

--- Assets/Scripts/VideoScript.cs
+++ Assets/Scripts/VideoScript.cs
@@ -6,10 +6,21 @@ public class VideoScript : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
     public string nextSceneName;
+    public string videoFileName;
 
     void Start()
     {
+        PlayVideo();
         videoPlayer.loopPointReached += OnVideoEnd;
+    }
+
+    public void PlayVideo(){
+        if (videoPlayer){
+            string videoPath = System.IO.Path.Combine(Application.streamingAssetsPath, videoFileName);
+            Debug.Log(videoPath);
+            videoPlayer.url = videoPath;
+            videoPlayer.Play();
+        }
     }
 
     void OnVideoEnd(VideoPlayer vp)
