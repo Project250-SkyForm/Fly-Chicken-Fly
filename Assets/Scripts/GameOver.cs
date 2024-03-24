3778a5eaf61e1fc9d0452a2d3c55d236de3f6b07
Jimmy <zhijianliu72@gmail.com>
2024-03-23 23:55:38 -0600

    update score ui

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public float elapsedTime;
+    public float duration;
+    public GameObject gameOver;
+    public GameObject score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.unscaledDeltaTime;
+        if (elapsedTime >= duration){
+            UIController.Instance.SetUINotActive(gameOver);
+            UIController.Instance.SetUIActive(score);
+        }
+    }
+}
