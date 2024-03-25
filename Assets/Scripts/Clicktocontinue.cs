1f0e1bfff2235879b3bf941fc6a67505ead7e5bf
Jimmy <zhijianliu72@gmail.com>
2024-03-24 21:15:38 -0600

    update new score clickable

---
+++ Assets/Scripts/Clicktocontinue.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clicktocontinue : MonoBehaviour
+{
+    public GameObject newScore;
+    public AudioSource audioSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            UIController.Instance.SetUINotActive(newScore);
+            audioSource.Play();
+        }
+    }
+}
